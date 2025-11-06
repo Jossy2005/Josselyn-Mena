1c3653e1f7839ed5e985dba3cabe6854908db202
Jossy2005 <jab.mena@yavirac.edu.ec>
2025-11-06 10:35:19 -0500

    Prueba

--- app.py
+++ app.py
@@ -1,14 +1,24 @@
 from flask import Flask
 
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def hello_world():
-    return '<h1>¡Hola desde mi aplicación!</h1><p>Esta es una app Flask muy básica.</p>'
+    return (
+        '<h1>¡Hola desde mi aplicación!</h1>'
+        '<p>Esta es una app Flask muy básica.</p>'
+    )
+
 
 @app.route('/saludo/<nombre>')
 def saludo_personalizado(nombre):
-    return f'<h2>¡Hola, {nombre}!</h2><p>Bienvenido a la aplicación Flask.</p>'
+    return (
+        f'<h2>¡Hola, {nombre}!</h2>'
+        '<p>Bienvenido a la aplicación Flask.</p>'
+    )
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
